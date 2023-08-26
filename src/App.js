d5a41ff24441d07f3b6b6aed98308c76442431d4
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-08-26 19:19:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route path={"/"} element={<Home />} />
-            <Route path={"/About"} element={<About />} />
-            <Route path={"/Projects"} element={<Projects />} />
-            <Route path={"/Contact"} element={<Contact />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
